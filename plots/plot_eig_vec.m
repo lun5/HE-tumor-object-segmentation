7e945c5e9cad81bddda286ffc3689a720fbe4a8a
luongn <luongnth@gmail.com>
2014-11-05 10:13:36 -0500

    example of spectral clustering went wrong

--- plots/plot_eig_vec.m
+++ plots/plot_eig_vec.m
@@ -12,10 +12,3 @@ figure; imagesc(E);
 axis equal;axis([0 im_sizes{1}(1) 0 im_sizes{1}(2)])
 set(gca,'xtick',[]);set(gca,'ytick',[]);
 colormap('gray');
-figure;
-for i =1:9
-    subplot(3,3,i); imagesc(vect(:,:,i));
-    axis equal;axis([0 ty 0 tx])
-    set(gca,'xtick',[]);set(gca,'ytick',[]);
-    colormap('gray')
-end
