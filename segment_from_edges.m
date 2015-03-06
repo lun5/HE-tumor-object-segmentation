29e03855f2fc07861e9aba2047a2d8ce476940a9
lun5 <luongnth@gmail.com>
2015-03-05 21:02:19 -0500

    finish boundary for pmi+phi psi

--- segment_from_edges.m
+++ segment_from_edges.m
@@ -1,6 +1,8 @@
 % script to process the segmentation
 tiles_dir = fullfile(pwd,'HEimages');
-imname = '9uixINHtjjiS.tif';
+%imname = '9uixINHtjjiS.tif';
+imname = '2ALe5NgRyfnpo.tif';
+
 %% result directory
 splitStr = regexp(imname,'\.','split');
 imresult_dir = fullfile(pwd,'results','HE_results',splitStr{1});
@@ -14,29 +16,29 @@ opts_affinity = setEnvironment_affinity;
 features = {'luminance','hue opp'};
 affinities = {'PMI','difference'};
 
-% for feature_iter = 1:length(features)
-%     for aff_iter = 1:length(affinities)
-%         opts_affinity.features.which_features = features{feature_iter};
-%         opts_affinity.affinityFunction = affinities{aff_iter};
-%         % put the rest in here. 
-%     end
-% end
+for feature_iter = 1:length(features)
+    for aff_iter = 1:length(affinities)
+        opts_affinity.features.which_features = features{feature_iter};
+        opts_affinity.affinityFunction = affinities{aff_iter};
+        % put the rest in here. 
+        which_features = opts_affinity.features.which_features
+        which_affinity = opts_affinity.affinityFunction
+        methodresult_dir = fullfile(imresult_dir,[which_features{1} '_' which_affinity]);
 
-which_features = opts_affinity.features.which_features
-which_affinity = opts_affinity.affinityFunction
-methodresult_dir = fullfile(imresult_dir,[which_features{1} '_' which_affinity]);
+        % load E_oriented
+        E_oriented = load(fullfile(methodresult_dir,'E_oriented.mat'));
+        E_oriented = E_oriented.data;
 
-% load E_oriented
-E_oriented = load(fullfile(methodresult_dir,'E_oriented.mat'));
-E_oriented = E_oriented.data;
-
-if (~ispc)
-        tic;thresh = 0.1;
-        E_ucm = contours2ucm_crisp_boundaries(E_oriented,opts_affinity, opts_clustering);
-        segmented_image = ucm2colorsegs(E_ucm,I,thresh);
-        figure;
-        subplot(121); imshow(uint8(I)); subplot(122); 
-        imshow(uint8(segmented_image));toc
-else
-        segmented_image = [];
+        if (~ispc)
+            tic;thresh = 0.1;
+            E_ucm = contours2ucm_crisp_boundaries(E_oriented,opts_affinity, opts_clustering);
+            segmented_image = ucm2colorsegs(E_ucm,I,thresh);
+            figure;
+            %subplot(121); imshow(uint8(I)); subplot(122); 
+            imshow(uint8(segmented_image));toc
+        else
+            segmented_image = [];
+        end
+    end
 end
+
