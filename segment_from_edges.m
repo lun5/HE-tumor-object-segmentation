ee5572030edb2162de966ee40d848b67b2ee5feb
lun5 <luongnth@gmail.com>
2015-03-05 21:59:55 -0500

    segmentation for 2ALe5

--- segment_from_edges.m
+++ segment_from_edges.m
@@ -23,22 +23,24 @@ for feature_iter = 1:length(features)
         % put the rest in here. 
         which_features = opts_affinity.features.which_features
         which_affinity = opts_affinity.affinityFunction
-        methodresult_dir = fullfile(imresult_dir,[which_features{1} '_' which_affinity]);
+        methodresult_dir = fullfile(imresult_dir,[which_features '_' which_affinity]);
 
         % load E_oriented
         E_oriented = load(fullfile(methodresult_dir,'E_oriented.mat'));
         E_oriented = E_oriented.data;
 
         if (~ispc)
-            tic;thresh = 0.1;
+            tic;thresh = 0.2;
             E_ucm = contours2ucm_crisp_boundaries(E_oriented,opts_affinity, opts_clustering);
             segmented_image = ucm2colorsegs(E_ucm,I,thresh);
-            figure;
+            h=figure;
             %subplot(121); imshow(uint8(I)); subplot(122); 
             imshow(uint8(segmented_image));toc
         else
             segmented_image = [];
         end
+        savefig(h,fullfile(methodresult_dir,['segmented_0' num2str(thresh*10) '.fig']));
+        clear segmented_image E_ucm E_oriented
     end
 end
 
