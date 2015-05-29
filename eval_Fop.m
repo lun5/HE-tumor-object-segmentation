0de11b05a58b7ca14f71bfcc676ab7d11f320b1d
luongn <luongnth@gmail.com>
2015-05-29 17:21:30 -0400

    add evaluation for objects and parts

--- eval_Fop.m
+++ eval_Fop.m
@@ -1,21 +1,30 @@
-%fname = '95f7k8LoesYevi.mat'; %
-%fname = 'aNaggwovpxANWq0.mat';
-gt_dir = '/Users/lun5/Research/data/gt_dir';
-seg_dir_all = '/Users/lun5/Research/data/segmentResults';
-%seg_dir = fullfile(seg_dir_all,'rho200sig25');
-seg_dir = fullfile(seg_dir_all,'rho300sig25');
-fnames = dir(fullfile(gt_dir,'*.mat'));
+% data_dir = '/Users/lun5/Research/data';
+% gt_dir = '/Users/lun5/Research/data/groundTruth/';
+% seg_dir_all = '/Users/lun5/Research/data/segmentResults';
+% seg_dir = fullfile(seg_dir_all,'eval_hue_512_512');
+% output_dir = fullfile(seg_dir_all,'512_512_segmentout_02');
+% if ~exist(output_dir,'dir')
+%     mkdir(output_dir);
+% end
+
+function [ Fop_ods, P_ods, R_ods, bestT, Fop_ois, P_ois, R_ois] = eval_Fop(gtDir, inDir, outDir)
+
+fnames = dir(fullfile(gtDir,'*.mat'));
 fnames =  {fnames.name}';
 thresh = 0.01:0.01:1;
-Fop_measure_thr = cell(length(fnames),1);
+%Fop_measure_thr = cell(length(fnames),1);
 Fop_measure_stat = zeros(length(thresh),4);
-Fop_measure_best = zeros(1,4);
 numImages = length(fnames);
+Fop_measure_img = zeros(numImages,5);
 parfor i = 1:numImages
-    tmp = load(fullfile(seg_dir,fnames{i})); 
+    tmp = load(fullfile(inDir,fnames{i})); 
     ucm = double(tmp.data); %clear tmp;
-    tmp = load(fullfile(gt_dir,fnames{i}));
+    tmp = load(fullfile(gtDir,fnames{i}));
     ground_truth = tmp.groundTruth{1,1}.Segmentation; %clear tmp;
+    %figure; imagesc(ground_truth); axis off; axis equal; set(gca,'Position',[0 0 1 1]);
+    %splitStr = regexp(fnames{i},'\.','split');
+    %filename = fullfile(output_dir,[splitStr{1} '_groundtruth.png']);
+    %print(gcf, '-dpng', filename);
     im_fop_th = zeros(length(thresh),4);
     for j = 1:length(thresh)
         labels2 = bwlabel(ucm <= thresh(j));
@@ -25,25 +34,55 @@ parfor i = 1:numImages
         im_fop_th(j,:) = cat(2, thresh(j), measure(1:3));
         %fprintf('thresh %1.3f Fop %1.2f %1.2f %1.2f\n',thresh(j),measure(1:3));
     end
-    Fop_measure_thr{i} = im_fop_th;
+    %Fop_measure_thr{i} = im_fop_th;
     Fop_measure_stat = Fop_measure_stat + im_fop_th./numImages;
     [~,ind_best] = max(im_fop_th(:,2));
-    Fop_measure_best = Fop_measure_best + im_fop_th(ind_best,:)./numImages;
+    %labels2 = bwlabel(ucm <= thresh(ind_best));
+    %labels2 = bwlabel(ucm <= 0.2);
+    %seg = labels2(2:2:end, 2:2:end);
+    %figure; imagesc(seg); axis equal; axis off;set(gca,'Position',[0 0 1 1]);
+    %filename = fullfile(output_dir,[splitStr{1} '_bestSeg.png']);
+    %print(gcf, '-dpng', filename);close all;
+    Fop_measure_img(i,:) = cat(2,i,im_fop_th(ind_best,:));
     i
 end
 
-[Fmax, ind] = max(Fop_measure_stat(:,2));
-Fop_measure_stat(ind,:)
-figure; plot(Fop_measure_stat(:,4),Fop_measure_stat(:,3));hold on;
-plot(Fop_measure_stat(ind,4), Fop_measure_stat(ind,3),'ro','MarkerSize',15,'MarkerFaceColor','r');hold off;
+Fop_measure_stat(:,2) = 2*(Fop_measure_stat(:,3).*Fop_measure_stat(:,4))./(Fop_measure_stat(:,3)+Fop_measure_stat(:,4));
+[Fop_ods, ind] = max(Fop_measure_stat(:,2));
+P_ods = Fop_measure_stat(ind,3); R_ods = Fop_measure_stat(ind,4); bestT = Fop_measure_stat(ind,1);
 
-Fop_measure_best(2) = 2*(Fop_measure_best(3)*Fop_measure_best(4))/(Fop_measure_best(3) + Fop_measure_best(4));
-Fop_measure_best
+Fop_measure_ois = mean(Fop_measure_img,1);
+Fop_ois = 2*(Fop_measure_ois(4)*Fop_measure_ois(5))/(Fop_measure_ois(4) + Fop_measure_ois(5));
+P_ois = Fop_measure_ois(4);R_ois = Fop_measure_ois(5);
+fname = fullfile(outDir,'eval_Fop.txt');
+fid = fopen(fname,'w');
+if fid==-1,
+   error('Could not open file %s for writing.',fname);
+end
+fprintf(fid,'%10g %10g %10g %10g %10g %10g %10g %10g\n',bestT,R_ods,P_ods,Fop_ods,R_ois,P_ois,Fop_ois);
+fclose(fid);
 
-Fop_measure_all = cat(1,Fop_measure_thr{1:end});
-[F_max_ois, ind_ois] = max(Fop_measure_all(:,2));
-Fop_measure_all(ind_ois,:)
+fname = fullfile(outDir,'eval_Fop_img.txt');
+fid = fopen(fname,'w');
+if fid==-1,
+    error('Could not open file %s for writing.',fname);
+end
+fprintf(fid,'%10d %10g %10g %10g %10g\n',Fop_measure_img');
+fclose(fid);
 
-Fop_best = Fop_measure_thr{32};
-figure; plot(Fop_best(:,4),Fop_best(:,3));hold on;
-plot(Fop_best(mod(ind_ois,100),4), Fop_best(mod(ind_ois,100),3),'ro','MarkerSize',15,'MarkerFaceColor','r');hold off;
+fname = fullfile(outDir,'eval_Fop_thr.txt');
+dlmwrite(fname, Fop_measure_stat,'delimiter','\t','precision',3);
+
+
+%open('isoF.fig'); hold on;
+%plot(Fop_measure_stat(:,4),Fop_measure_stat(:,3),'LineWidth',3);%hold on;
+%plot(Fop_measure_stat(ind,4), Fop_measure_stat(ind,3),'ro','MarkerSize',15,'MarkerFaceColor','r');hold off;
+%plot_eval(outDir);
+
+%Fop_measure_all = cat(1,Fop_measure_thr{1:end});
+%[F_max_ois, ind_ois] = max(Fop_measure_all(:,2));
+%Fop_measure_all(ind_ois,:)
+
+%Fop_best = Fop_measure_thr{32};
+%figure; plot(Fop_best(:,4),Fop_best(:,3));hold on;
+%plot(Fop_best(mod(ind_ois,100),4), Fop_best(mod(ind_ois,100),3),'ro','MarkerSize',15,'MarkerFaceColor','r');hold off;
--- toolboxes/BSR/benchmarks/plot_eval.m
+++ toolboxes/BSR/benchmarks/plot_eval.m
@@ -15,17 +15,39 @@ if exist(fullfile(evalDir,'eval_bdry_thr.txt'),'file'),
 
 
     evalRes = dlmread(fullfile(evalDir,'eval_bdry.txt'));
-    if size(prvals,1)>1,
+    %if size(prvals,1)>1,
         plot(prvals(1:end,2),prvals(1:end,3),col,'LineWidth',3);
-    else
+    %else
         plot(evalRes(2),evalRes(3),'o','MarkerFaceColor',col,'MarkerEdgeColor',col,'MarkerSize',8);
-    end
+    %end
     hold off
 
     fprintf('Boundary\n');
     fprintf('ODS: F( %1.2f, %1.2f ) = %1.2f   [th = %1.2f]\n',evalRes(2:4),evalRes(1));
     fprintf('OIS: F( %1.2f, %1.2f ) = %1.2f\n',evalRes(5:7));
     fprintf('Area_PR = %1.2f\n\n',evalRes(8));
+end
+
+if exist(fullfile(evalDir,'eval_Fop_thr.txt'),'file'),
+    open('isoF.fig');
+    hold on
+    prvals = dlmread(fullfile(evalDir,'eval_Fop_thr.txt')); % thresh,r,p,f
+    f=find(prvals(:,2)>=0.01);
+    prvals = prvals(f,:);
+
+
+    evalRes = dlmread(fullfile(evalDir,'eval_Fop.txt'));
+    %if size(prvals,1)>1,
+        plot(prvals(:,4),prvals(:,3),col,'LineWidth',3);
+    %else
+        plot(evalRes(2),evalRes(3),'o','MarkerFaceColor',col,'MarkerEdgeColor',col,'MarkerSize',10);
+    %end
+    hold off
+
+    fprintf('Objects and parts\n');
+    fprintf('ODS: F( %1.2f, %1.2f ) = %1.2f   [th = %1.2f]\n',evalRes(2:4),evalRes(1));
+    fprintf('OIS: F( %1.2f, %1.2f ) = %1.2f\n',evalRes(5:7));
+    %fprintf('Area_PR = %1.2f\n\n',evalRes(8));
 end
 
 if exist(fullfile(evalDir,'eval_cover.txt'),'file'),
