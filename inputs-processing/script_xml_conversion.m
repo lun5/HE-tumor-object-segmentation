10345c6301c92af513ac0a2bf63e959136d02570
lun5 <lun5@pitt.edu>
2015-08-13 09:20:47 -0400

    fix the conversion from ImageScope xml => labelME xml

--- inputs-processing/script_xml_conversion.m
+++ inputs-processing/script_xml_conversion.m
@@ -11,14 +11,19 @@ HOMEIMAGES = fullfile(data_dir,'Images'); % you can set here your default folder
 HOMEANNOTATIONS = fullfile(data_dir,'Annotations'); % you can set here your default folder
 HOMELMSEGMENTS = fullfile(data_dir,'groundTruth');
 
-xmlfilename = '2ALe5NgRyfnpo.xml';
-folder_name = 'best_images_july30';
-new_xml = xmlLabelMe(fullfile(data_dir,'Annotations_ImageScope',xmlfilename),folder_name,'luong');
+xmlfilename = 'jbaKL4TsEqT.xml';
 
-fileId = fopen(fullfile(HOMEANNOTATIONS,folder_name,xmlfilename),'w');
-fprintf(fileId,'%s',new_xml);
-fclose(fileId);
+filenames = dir(fullfile(data_dir,'Annotations_ImageScope','*.xml'));
+filenames = {filenames.name}';
+folder_name = 'best_images_july30';
+parfor i = 1:length(filenames)
+    xmlfilename = filenames{i};
+    new_xml = xmlLabelMe(fullfile(data_dir,'Annotations_ImageScope',xmlfilename),folder_name,'luong');
+    fileId = fopen(fullfile(HOMEANNOTATIONS,folder_name,xmlfilename),'w');
+    fprintf(fileId,'%s',new_xml);
+    fclose(fileId);
+end
 
 database = LMdatabase(fullfile(HOMEANNOTATIONS,folder_name));
 [D,j] = LMquery(database, 'folder',folder_name);
-[img, seg, names] = LM2segments(D(1), [], HOMEIMAGES, HOMELMSEGMENTS);
+[img, seg, names] = LM2segments(D(11), [], HOMEIMAGES, HOMELMSEGMENTS);
--- inputs-processing/xmlLabelMe.m
+++ inputs-processing/xmlLabelMe.m
@@ -21,22 +21,35 @@ IndexC = strfind(xml_lines,'Region Id=');
 Index = find(not(cellfun('isempty', IndexC)));
 IndexC = strfind(xml_lines,'Plots');
 End_Index = find(not(cellfun('isempty',IndexC)));
-for i = 1:length(Index)
+Index(length(Index) + 1) = End_Index;
+for i = 1:length(Index)-1
     s = strsplit(xml_lines{Index(i)},' ');
     indx = strfind(s,'Text=');
     indx = find(not(cellfun('isempty',indx)));
     object_name = strsplit(strrep(s{indx},'"',''),'=');
     object_name = strcat(object_name{2:end});
     xml_str = sprintf(['\n<object>\n <name>%s</name>\n<deleted>0</deleted>\n'...
-        '<verified>0</verified>\n <occluded>no</occluded>\n <attributes />\n' ...
+        '<verified>0</verified>\n <occluded>no</occluded>\n'],object_name);
+    % attribute
+    tmp = strfind(xml_lines(Index(i):Index(i+1)),'Attribute Name');
+    indx_attribute = find(not(cellfun('isempty',tmp)));
+    if ~isempty(indx_attribute)
+        tmp = xml_lines(indx_attribute + Index(i) - 1);
+        tmp = strsplit(strrep(tmp{1},'"',''),'Value=');
+        attribute = strrep(tmp{2},';','_');
+        xml_str = strcat(xml_str,sprintf(['<attributes>%s</attributes>\n' ...
         '<parts><hasparts /> <ispartof /> </parts>\n<date></date>\n'...
-        '<id>%d</id>\n<polygon>\n<username>%s</username>\n'],object_name,i-1,source);
-    if i < length(Index)
-        end_indx = Index(i+1)-1;
+        '<id>%d</id>\n<polygon>\n<username>%s</username>\n'],attribute,i-1,source));
     else
-        end_indx = End_Index -1;
+        xml_str = strcat(xml_str,sprintf(['<attributes />\n' ...
+        '<parts><hasparts /> <ispartof /> </parts>\n<date></date>\n'...
+        '<id>%d</id>\n<polygon>\n<username>%s</username>\n'],i-1,source));  
     end
-    for j = (Index(i)+3) : end_indx
+    
+    end_indx = Index(i+1)-1;
+    tmp = strfind(xml_lines(Index(i):Index(i+1)),'Vertices');
+    indx_vertices = find(not(cellfun('isempty',tmp)));
+    for j = (Index(i)+indx_vertices) : end_indx
        coords = xml_lines{j};
        coords = strrep(coords,'"','');
        coords = strrep(coords,'Vertex ','');
