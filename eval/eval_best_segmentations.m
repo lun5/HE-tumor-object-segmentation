23cdce6cb7ca62679d4b0b39e619aa924aac651c
lun5 <lun5@pitt.edu>
2015-10-12 23:10:52 -0400

    print out best segmentation results for each image

--- eval/eval_best_segmentations.m
+++ eval/eval_best_segmentations.m
@@ -1,66 +1,65 @@
 % generate output at OIS for best images in the test set
 % Luong Nguyen 07/23/2015
 % this is to test why the performanc isn't very good
-% DATA_DIR ='/home/lun5/HEproject/'; % linux
-% %IMG_DIR = fullfile(DATA_DIR,'data','images','test');
-% GT_DIR = fullfile(DATA_DIR,'data','groundTruth_512_512');
-% %IMG_DIR = '/home/lun5/HEproject/TilesForLabeling_tiff_renamed';
-% IMG_DIR = '/home/lun5/HEproject/data/images/test';
-% RESULTS_DIR = fullfile(DATA_DIR,'evaluation_results','combinedFeatures_32images');
+% UPDATE: 10/12/15
 
-githubdir = '/Users/lun5/Research/github/HE-tumor-object-segmentation';
-addpath(genpath(githubdir));
-seismdir = '/Users/lun5/Research/github/seism'; addpath(genpath(seismdir));
-cd(githubdir)
-DATA_DIR = '/Users/lun5/Research/data/';
-IMG_DIR = fullfile(DATA_DIR,'TilesForLabeling_tiff_renamed','test');%'/home/lun5/HEproject/data/images/test';
-GT_DIR = fullfile(DATA_DIR,'groundTruth','groundTruth_512_512_reannotated','best_images_july30');%fullfile(DATA_DIR,'data','groundTruth_512_512');
-RESULTS_DIR = fullfile(DATA_DIR,'evaluation_results','eval_reannotated');
-gt_display_old = fullfile(DATA_DIR,'groundTruth','groundTruth_512_display');
-gt_display_new = fullfile(DATA_DIR,'groundTruth','groundTruth_reannotated_display');
-outDir = fullfile(RESULTS_DIR,'outputTop_015');
+%% mac
+% githubdir = '/Users/lun5/Research/github/HE-tumor-object-segmentation';
+% addpath(genpath(githubdir)); cd(githubdir)
+% seismdir = '/Users/lun5/Research/github/seism'; addpath(genpath(seismdir));
+% DATA_DIR = '/Users/lun5/Research/data/';
+% IMG_DIR = fullfile(DATA_DIR,'TilesForLabeling_tiff_renamed','test');%'/home/lun5/HEproject/data/images/test';
+% GT_DIR = fullfile(DATA_DIR,'groundTruth','groundTruth_512_512_reannotated','best_images_july30');%fullfile(DATA_DIR,'data','groundTruth_512_512');
+% RESULTS_DIR = fullfile(DATA_DIR,'evaluation_results','eval_reannotated');
+%% window
+githubdir = 'C:\Users\luong_nguyen\Documents\GitHub\HE-tumor-object-segmentation'; % window
+seismdir = 'C:\Users\luong_nguyen\Documents\GitHub\seism'; 
+addpath(genpath(seismdir)); cd(githubdir)
+DATA_DIR = 'Z:\';
+IMG_DIR = 'Z:\Tiles_512\Test';
+GT_DIR = 'Z:\HEproject\data\groundTruth_512_512';
+RESULTS_DIR = fullfile(DATA_DIR,'HEproject','evaluation_results','Isola_lowres_accurate');
+gt_display = fullfile(DATA_DIR,'groundTruth','groundTruth_reannotated_display');
+outDir = fullfile(RESULTS_DIR,'best_boundary');
+
 if ~exist(outDir,'dir')
     mkdir(outDir)
 end
 
-eval_img = dlmread(fullfile(RESULTS_DIR,'ev_txt','eval_bdry_img.txt'));
+eval_img = dlmread(fullfile(RESULTS_DIR,'ev_txt_reannotated','eval_bdry_img.txt'));
 %eval_img = dlmread(fullfile(RESULTS_DIR,'ev_txt','eval_Fop_img.txt'));
-ind_top = 1:41;%find(eval_img(:,5) > 0.5);
-best_thres = eval_img(ind_top,2);
+best_thres = eval_img(:,2);
 IMG_EXT = '.tif';
 img_list = dirrec(IMG_DIR,IMG_EXT);
-img_list = img_list(:,ind_top);
-weight_str = 'weights_3_1_1';
-outDir_wt = fullfile(outDir,[weight_str,'_bdry']);
-if ~ exist(outDir_wt,'dir')
-    mkdir(outDir_wt)
+if ~ exist(outDir,'dir')
+    mkdir(outDir)
 end
+
 parfor i = 1:numel(img_list)
     [~,im_name,~] = fileparts(img_list{i});   
     fprintf('\n\nCalculate best segmentation for image %s...',im_name); T = tic;
-    if ~exist(fullfile(outDir_wt,[im_name '_bestSeg.tif']),'file')
-        tmp = load(fullfile(RESULTS_DIR,'ucm2',weight_str,[im_name '.mat']));
-        ucm2 = tmp.data;ucm2 = ucm2(3:2:end,3:2:end);
-        I = imread(img_list{i});I = double(I(1:4:end,1:4:end,:));
-        thr = 0.15; %best_thres(i);
-        segmented_image = ucm2colorsegs(ucm2,I,thr);
-        imwrite(uint8(segmented_image),fullfile(outDir_wt,[im_name '_bestSeg.tif']));%,'Resolution',300);
-        %J = label2rgb(bwlabel(ucm2(2:end,2:end) <= thr),'jet');
-        %imwrite(J,fullfile(outDir_wt,[im_name,'_bestSeg_colr.tif']),'Resolution',300);
-    end
-    
-    filenames = {img_list{i},...
-        fullfile(outDir_wt,[im_name '_bestSeg.tif']),...
-        fullfile(gt_display_old,[lower(im_name) '.bmp']),...
-        fullfile(gt_display_new,[lower(im_name) '.bmp'])};
-        %fullfile(outDir_wt,[im_name '_bestSeg_colr.tif']),...
-        %fullfile(gt_display_old,[lower(im_name) '.tif'])};
-    a = cell(4,1);
-    for j = 1:length(filenames)
-        a{j} = imread(filenames{j});
-    end
-    a{1} = a{1}(1:4:end,1:4:end,:);a{2} = a{2}(1:2:end, 1:2:end,:);
-    montage = cat(2,a{:});
-    imwrite(montage,fullfile(outDir_wt,[im_name '_montage.tif']));%,'Resolution',300);
+    outFile = fullfile(outDir,[im_name, '.jpg']);
+    if ~exist(outFile,'file')
+        tmp = load(fullfile(RESULTS_DIR,'ucm2',[im_name '.mat']));
+        ucm2 = tmp.data; ucm2 = ucm2(3:2:end,3:2:end);
+        I = imread(img_list{i});
+        thr = best_thres(i);
+        edge_map = (ucm2>=thr);        
+        edge_map = imdilate(edge_map, strel('disk',1));
+        edge_map_im = I.*uint8(repmat(~edge_map,[1 1 3]));
+        imwrite(edge_map_im,outFile);
+        %imwrite(label2rgb(labels),fullfile(output_dir,'seg_im',[im_name, '_' num2str(q_thresh), '_seg.jpg']));
+    end    
+%     filenames = {img_list{i},...
+%         fullfile(outDir,[im_name '_bestSeg.tif']),...
+%         fullfile(gt_display_old,[lower(im_name) '.bmp']),...
+%         fullfile(gt_display,[lower(im_name) '.bmp'])}
+%     a = cell(4,1);
+%     for j = 1:length(filenames)
+%         a{j} = imread(filenames{j});
+%     end
+%     a{1} = a{1}(1:4:end,1:4:end,:);a{2} = a{2}(1:2:end, 1:2:end,:);
+%     montage = cat(2,a{:});
+%     imwrite(montage,fullfile(outDir,[im_name '_montage.tif']));%,'Resolution',300);
     t = toc(T); fprintf('done: %1.2f sec\n', t);
 end
--- inputs-processing/script_xml_conversion.m
+++ inputs-processing/script_xml_conversion.m
@@ -11,19 +11,62 @@ HOMEIMAGES = fullfile(data_dir,'Images'); % you can set here your default folder
 HOMEANNOTATIONS = fullfile(data_dir,'Annotations'); % you can set here your default folder
 HOMELMSEGMENTS = fullfile(data_dir,'groundTruth');
 
-xmlfilename = 'jbaKL4TsEqT.xml';
-
+%xmlfilename = 'jbaKL4TsEqT.xml';
 filenames = dir(fullfile(data_dir,'Annotations_ImageScope','*.xml'));
 filenames = {filenames.name}';
 folder_name = 'best_images_july30';
+
 parfor i = 1:length(filenames)
     xmlfilename = filenames{i};
-    new_xml = xmlLabelMe(fullfile(data_dir,'Annotations_ImageScope',xmlfilename),folder_name,'luong');
-    fileId = fopen(fullfile(HOMEANNOTATIONS,folder_name,xmlfilename),'w');
-    fprintf(fileId,'%s',new_xml);
-    fclose(fileId);
+    new_xmlfilename = fullfile(HOMEANNOTATIONS,folder_name,xmlfilename);
+    if ~exist(new_xmlfilename,'file')
+        T = tic;
+        new_xml = xmlLabelMe(fullfile(data_dir,'Annotations_ImageScope',xmlfilename),folder_name,'luong');
+        fileId = fopen(fullfile(HOMEANNOTATIONS,folder_name,xmlfilename),'w');
+        fprintf(fileId,'%s',new_xml);
+        fclose(fileId);
+        elapsed_time = toc(T); fprintf('\n Done with %s in %.1f seconds\n',xmlfilename, elapsed_time);
+    end
 end
 
 database = LMdatabase(fullfile(HOMEANNOTATIONS,folder_name));
 [D,j] = LMquery(database, 'folder',folder_name);
-[img, seg, names] = LM2segments(D(11), [], HOMEIMAGES, HOMELMSEGMENTS);
+
+bdry_im_dir = fullfile(data_dir,'bdry_im');
+seg_im_dir = fullfile(data_dir,'segmentation_display');
+if ~exist(bdry_im_dir,'dir')
+    mkdir(bdry_im_dir);
+end
+
+if ~exist(seg_im_dir,'dir')
+    mkdir(seg_im_dir);
+end
+Nimages = length(D);
+parfor ndx = 1:Nimages
+    if ~isfield(D(ndx).annotation, 'object')
+        continue; 
+    end
+    im_name = D(ndx).annotation.filename(1:end-4);
+    fileseg = fullfile(HOMELMSEGMENTS, D(ndx).annotation.folder, [im_name '.mat']);
+    if ~exist(fileseg,'file')
+        T = tic;
+        [img, seg, names] = LM2segments(D(ndx), [], HOMEIMAGES, HOMELMSEGMENTS);
+        mult = 4;
+        seg = seg(1:mult:end,1:mult:end);img = imresize(img,size(seg));
+        %[annotation, img] = LMread(D,ndx,HOMEIMAGES);
+        % plot the annotations %figure;LMplot(annotation, img)
+        bmap = logical(seg2bdry(seg,'imageSize'));  %figure; imshow(mat2gray(bmap));
+        groundTruth = cell(1,1);
+        groundTruth{1,1} = struct('Segmentation',seg,'Boundaries',bmap);
+        groundTruth{1,1}.names = names;
+        parsave_custom(fileseg,groundTruth,'groundTruth');
+        % change thickness of edges
+        edge_map = imdilate(bmap, strel('disk',1));
+        edge_map_im = img.*uint8(repmat(~edge_map,[1 1 3]));
+        imwrite(uint8(edge_map_im),fullfile(bdry_im_dir,[im_name, '_bdry.tif']),'Resolution',300);
+        imwrite(uint8(label2rgb(seg)),fullfile(seg_im_dir,[im_name, '_seg.tif']),'Resolution',300);
+        elapsed_time = toc(T); fprintf('\n Done with %s in %.1f seconds\n',im_name, elapsed_time);
+    end
+end
+
+disp('Done');
